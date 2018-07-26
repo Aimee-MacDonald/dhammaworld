ba0f1d5b40be1659feee711627824be8cc8da022
Aimee MacDonald <aimeelmacdonald@gmail.com>
2018-07-26 20:50:40 +0100

    Temporary landing page

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.set("views", __dirname + "/views");
 app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/static"));
+
+app.set("public", __dirname + "/static");
 
 app.get("/", (req, res) => {
   res.render("index");
