56513d96efbd6b1749f2d7eaab66492812a07c72
Aimee <aimeelmacdonald@gmail.com>
2018-08-27 13:30:35 +0200

    Login/Register Animations

--- static/scripts/dev.js
+++ static/scripts/dev.js
@@ -79,3 +79,57 @@ function resetState(){
       resetState();
   }
 }
+
+var registrationFlag = false;
+function toggleLoginRegister(){
+  registrationFlag = !registrationFlag;
+  var loginButton = document.getElementById("login-button");
+  var registerButton = document.getElementById("register-button");
+  var loginClip = loginButton.getBoundingClientRect();
+  var registerClip = registerButton.getBoundingClientRect();
+
+  if(registrationFlag){
+    loginButton.style.order = 1;
+    registerButton.style.order = -1;
+    document.getElementById("confirm-password").style.height = "auto";
+    loginButton.setAttribute("onclick", "toggleLoginRegister()");
+    registerButton.setAttribute("onclick", "submitRegister()");
+  } else {
+    loginButton.style.order = -1;
+    registerButton.style.order = 1;
+    document.getElementById("confirm-password").style.height = "0";
+    loginButton.setAttribute("onclick", "submitLogin()");
+    registerButton.setAttribute("onclick", "toggleLoginRegister()");
+  }
+
+  var loginClip2 = loginButton.getBoundingClientRect();
+  var registerClip2 = registerButton.getBoundingClientRect();
+  var loginDelta = loginClip.top - loginClip2.top;
+  var registerDelta = registerClip.top - registerClip2.top;
+
+  loginButton.animate([
+    {transform: 'translateY(' + loginDelta + 'px)'},
+    {transform: 'translateY(0px)'}
+  ], {
+    duration: 500,
+    iterations: 1,
+    fill: "forwards"
+  });
+
+  registerButton.animate([
+    {transform: 'translateY(' + registerDelta + 'px)'},
+    {transform: 'translateY(0px)'}
+  ], {
+    duration: 500,
+    iterations: 1,
+    fill: "forwards"
+  });
+}
+
+function submitLogin(){
+  console.log("Submit Login");
+}
+
+function submitRegister(){
+  console.log("Submit Register");
+}
