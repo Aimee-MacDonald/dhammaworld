7a518262c141d2feccd5551030370a14c8b55801
Aimee <aimeelmacdonald@gmail.com>
2018-09-03 14:11:34 +0200

    Users basic database

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const User = require(__dirname + "/dbmodels/user");
+
+mongoose.connect(process.env.DBURL);
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
@@ -8,6 +14,13 @@ app.use(express.static(__dirname + "/static"));
 app.set("public", __dirname + "/static");
 
 app.get("/", (req, res) => {
+  
+  console.log("Creating new User:\n");
+  let newUser = new User({'email': 'email', 'password': 'password'});
+  newUser.save(err => {
+    if(err) console.log("Error: " + err);
+  });
+
   res.render("index");
 });
 
